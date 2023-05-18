7edde44fbd70213c1c5ba1b6fe01be4430321f6d
Liumingxun <liumingxun@yeah.net>
2023-05-18 16:22:58 +0800

    feat: customize

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Luoming 的奇妙基地';
+export const SITE_DESCRIPTION = '可能出现各种各样的东西，包括但不限于代码、摄影、游戏……';
