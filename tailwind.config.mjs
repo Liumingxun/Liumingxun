25214362422c3c9e8e593b470ff665aba39f6756
liumingxun <liumingxun@yeah.net>
2024-02-01 11:22:05 +0800

    chore: update iconfont

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,9 +5,7 @@ import themes from 'daisyui/src/theming/themes'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	safelist: ['dark'],
 	theme: {
 		fontFamily: {
 			sans: ['Fira Sans', ...fontFamily.sans],
