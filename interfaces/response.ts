e6a5a03b2d382fb9ad7f1974fde156ef8e69da8c
Liumingxun <liumingxun@yeah.net>
2024-09-06 11:24:11 +0800

    chore: clean code

--- interfaces/response.ts
+++
@@ -1,9 +0,0 @@
-export default interface Response<D> {
-    data: Array<D>,
-    meta: Record<string, unknown>,
-    error?: Record<string, unknown>,
-}
-
-export interface Wrapped<D> { 
-    data: D | null
-}
--- src/utils/buildSeo.ts
+++ src/utils/buildSeo.ts
@@ -1,10 +1,9 @@
-import type Image from "/interfaces/image"
 import type Seo from "/interfaces/seo"
 
 interface SeoParams {
     title: string,
     description: string,
-    image?: Image | string
+    image?: string
     tags?: string[],
     createdAt?: string,
     updatedAt?: string
@@ -12,19 +11,11 @@ interface SeoParams {
     noindex?: boolean
 }
 
-const isImage = (image: Image | string): image is Image => {
-    return typeof image !== "string"
-}
-
 export function buildSeo({ title, description, image, tags, createdAt, updatedAt, publishedAt, noindex = false }: SeoParams): Seo {
-    if (image && isImage(image)) { // @todo: image
-        image = image.attributes.url
-    }
-
     const seo: Seo = {
         title,
         description,
-        image: image as string,
+        image: image,
         createdAt,
         updatedAt,
         publishedAt,
