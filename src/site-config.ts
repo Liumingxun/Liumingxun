6834ac04f8b395cd9506b64d6f689ab9373b0483
Liumingxun <liumingxun@yeah.net>
2024-09-07 16:15:15 +0800

    feat: add projects page

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  title: 'Lime\'s Site - lolo洛明',
+  title: 'Lime\'s Site - 洛明Lime',
   description: '诶嘿！这里是洛明，你也可以叫我 Lime！是前端开发者，对好多新鲜事物感兴趣，站内可能会分享一些技术文章等，希望你能喜欢！',
   image: '/favicon.svg',
   author: '洛明Lime',
@@ -14,6 +14,10 @@ export const NAV_ITEMS: NavItem[] = [
   {
     path: '/blogs',
     title: 'blogs',
+  },
+  {
+    path: '/projects',
+    title: 'projects',
   },
   {
     path: '/about',
