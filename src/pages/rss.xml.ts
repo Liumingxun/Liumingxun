b4858a03885af5eefab96b5225bab26d6125bd88
Lime <liumingxun@yeah.net>
2025-03-22 19:42:21 +0800

    chore: lint

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,9 @@
+import type { RSSFeedItem } from '@astrojs/rss'
+import type { APIRoute } from 'astro'
 import { SITE } from '@/site-config'
 import { getFileCommitDate } from '@/utils/getCommitMeta'
-import rss, { type RSSFeedItem } from '@astrojs/rss'
+import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
-import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = async (context) => {
   const blogs = await getCollection('blog')
